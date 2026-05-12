55e3f7ddc2c23e5265056de0894996022411401e
MohamedMagdy277 <mm7423942@gmail.com>
2026-05-13 00:27:12 +0300

    Add responsive mobile layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import "./mobile.css";
 import App from "./pages/App.jsx";
 import { LanguageProvider } from "./context/LanguageContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
@@ -13,5 +14,5 @@ createRoot(document.getElementById("root")).render(
         <App />
       </LanguageProvider>
     </ThemeProvider>
-  </StrictMode>
+  </StrictMode>,
 );
